63c1cf0360975abcedcd77fd79488bede14f4c3f
Rida2504 <54786338+Rida2504@users.noreply.github.com>
2023-02-18 09:30:42 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run PES1UG20CS335\n";
+  cout << "Change is made\n";
   return 0;
 }
